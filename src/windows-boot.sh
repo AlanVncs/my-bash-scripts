a8c1ca5d9643255c18c30ed63c380948df0ee5fb
Alan Vinicius Silva <alanvinicios@gmail.com>
2020-01-29 15:53:47 -0300

    Script para bootar o windows

---
+++ src/windows-boot.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Autor: alanvinicios@gmail.com
+# Data: 29/01/2020
+# Descrição: Reinicia o computador e boota no Windows
+#            É necessário configurar o computador antes
+
+
+sudo grub-reboot 1 && sudo reboot
